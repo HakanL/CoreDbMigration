2d3cf6c04b1171406f03889ce5d8265910d048ac
Hakan Lindestaf <hakan@lindestaf.com>
2019-03-21 20:24:29 -0700

    Add project files.

---
+++ CoreDbMigration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreDbMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
